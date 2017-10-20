30176672b520290dec0a0fd61b7d01d20fef960d
= <Shawn.coding.acc@gmail.com>
2017-10-19 19:03:59 -0700

    edit

--- codefights/minesweeper/test.py
+++ codefights/minesweeper/test.py
@@ -33,38 +33,19 @@ def minesweeper(matrix):
   return m
 
 
-def minesweeper(matrix):
-  nrow = len(matrix)
-  ncol = len(matrix[0])
-  m = [[0 for j in range(ncol)] for i in range(nrow)]
-  
-  for i in range(nrow):
-    for j in range(ncol):
-      # Top left
-      if i-1 >= 0 and j-1 >= 0 and matrix[i-1][j-1]:
-        m[i][j] += 1
-      # Top
-      if j-1 >= 0 and matrix[i][j-1]:
-        m[i][j] += 1
-      # Top right
-      if i+1 < len(matrix) and j > 0 and matrix[i+1][j-1]:
-        m[i][j] += 1
-      # Left
-      if i-1 >= 0 and matrix[i-1][j]:
-        m[i][j] += 1
-      # Right
-      if i+1 < len(matrix) and matrix[i+1][j]:
-        m[i][j] += 1
-      # Bottom left
-      if i-1 >= 0 and j+1 < len(matrix[i]) and matrix[i-1][j+1]:
-        m[i][j] += 1
-      # Bottom
-      if j+1 < len(matrix[i]) and matrix[i][j+1]:
-        m[i][j] += 1
-      # Bottom right
-      if i+1 < len(matrix) and j+1 < len(matrix[i]) and matrix[i+1][j+1]:
-        m[i][j] += 1
-  return m
+def minesweepe(matrix):
+  r = []
+  for i in range(len(matrix)):
+    r.append([])
+    for j in range(len(matrix[0])):
+      l = -matrix[i][j]
+      for x in [-1, 0, 1]:
+        for y in [-1, 0, 1]:
+          if 0 <= i+x < len(matrix) and 0 <= j+y < len(matrix[0]):
+            l += matrix[i+x][j+y]
+      r[i].append(l)
+  return r
+
 
 
 # test 1
