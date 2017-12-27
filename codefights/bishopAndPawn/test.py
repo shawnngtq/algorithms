39c924786a68651097af09226e516b542deaabfb
Shawn <shawn.coding.acc@gmail.com>
2017-12-27 08:09:28 +0800

    bishop and pawn)

--- codefights/bishopAndPawn/test.py
+++ codefights/bishopAndPawn/test.py
@@ -1,7 +1,7 @@
-def bishopAndPawn(b, p):
-    b=[ord(b[0]),int(b[1])]
-    p=[ord(p[0]),int(p[1])]
-    return b[1]-b[0]==p[1]-p[0] or sum(b)==sum(p)
+def bishopAndPawn(bishop, pawn):
+    bishop=[ord(bishop[0]),int(bishop[1])]
+    pawn=[ord(pawn[0]),int(pawn[1])]
+    return bishop[1]-bishop[0]==pawn[1]-pawn[0] or sum(bishop)==sum(pawn)
 
 
 def bishopAndPawn(bishop, pawn):
