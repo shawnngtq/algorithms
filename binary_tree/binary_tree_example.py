1ff8eee799a1e80f8db405490fa7e27a1faabc5c
shawnngtq <shawn.coding.acc@gmail.com>
2021-04-21 11:04:22 +0800

    Update binary_tree/binary_tree_example.py

--- binary_tree/binary_tree_example.py
+++ binary_tree/binary_tree_example.py
@@ -24,13 +24,55 @@ class Node:
     def print_tree(self):
         if self.left:
             self.left.print_tree()
-        print(self.data),
+        print(self.data)
         if self.right:
             self.right.print_tree()
 
+    def in_order_traversal(self, root):
+        """left -> root -> right"""
+        res = []
+        if root:
+            res = self.in_order_traversal(root.left)
+            res.append(root.data)
+            res += self.in_order_traversal(root.right)
+        return res
 
-root = Node(12)
-root.insert(6)
+    def pre_order_traversal(self, root):
+        """root -> left -> right"""
+        res = []
+        if root:
+            res.append(root.data)
+            res += self.pre_order_traversal(root.left)
+            res += self.pre_order_traversal(root.right)
+        return res
+
+    def post_order_traversal(self, root):
+        """left -> right -> root"""
+        res = []
+        if root:
+            res = self.post_order_traversal(root.left)
+            res += self.post_order_traversal(root.right)
+            res.append(root.data)
+        return res
+
+
+root = Node(27)
 root.insert(14)
-root.insert(3)
+root.insert(35)
+root.insert(10)
+root.insert(19)
+root.insert(31)
+root.insert(42)
+root.insert(20)
+
+print("Print tree:")
 root.print_tree()
+
+print("In order traversal:")
+print(root.in_order_traversal(root))
+
+print("Pre order traversal:")
+print(root.pre_order_traversal(root))
+
+print("Post order traversal:")
+print(root.post_order_traversal(root))
